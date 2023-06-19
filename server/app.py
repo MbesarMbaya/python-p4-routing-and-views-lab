0b18d225d3b175c9e42d1cfd56e358acf48d25ba
Roy Mbaya <roymbaya08@gmail.com>
2023-06-19 12:28:46 +0300

    done

--- server/app.py
+++ server/app.py
@@ -18,26 +18,27 @@ def print_string(string):
 
 @app.route('/count/<int:num>')
 def count(num):
-    numbers = '<br>'.join(str(i) for i in range(1, num + 1))
+    numbers = '\n'.join(str(i) for i in range(1, num + 1))
     return numbers
 
 
-@app.route('/math/<int:num1><operation><int:num2>')
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
 def math(num1, operation, num2):
     if operation == '+':
-        result = num1 + num2
+        result = str(num1 + num2)
     elif operation == '-':
-        result = num1 - num2
+        result = str(num1 - num2)
     elif operation == '*':
-        result = num1 * num2
+        result = str(num1 * num2)
     elif operation == 'div':
-        result = num1 / num2
+        result = str(num1 / num2)
     elif operation == '%':
-        result = num1 % num2
+        result = str(num1 % num2)
     else:
         return 'Invalid operation'
 
-    return str(result)
+    return result
+
 
 if __name__ == '__main__':
     app.run()
